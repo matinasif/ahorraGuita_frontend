47463274b965bbb23db70a6ea00de5a3163a511d
matinasif <nasif.matias@gmail.com>
2024-09-19 15:10:10 -0300

    pruebas en APP.JS

--- src/App.js
+++ src/App.js
@@ -1,11 +1,25 @@
 import logo from './logo.svg';
 import './App.css';
 
+function retornarNumeroAleatorio(){
+  return Math.trunc(Math.random*10);
+}
 function App() {
+  const siglo = 21;
+  const persona ={
+    nombre: 'Matías',
+    edad: '28'
+  }
   return (
     <div>
-      <h1>Hola Mundo!</h1>
-      <p>Hola mundo parrafo</p>
+      <h1>Titulo nivel 1</h1>
+      <hr />
+      <p>Estamos en el siglo {siglo}</p>
+      <hr />
+      <p>Mi nombre es {persona.nombre} y mi edad {persona.edad}</p>
+      <hr />
+      <p>un numero aleatorio es </p>{retornarNumeroAleatorio()}
+
    </div>
 
   );
