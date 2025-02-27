3938019dd3c4657349a2e64cba4039c1ad1fa25e
ecaroa <elianacarolinaa@gmail.com>
2025-02-26 21:24:32 -0300

    se peude agregar fotos

--- src/components/ProductCard.js
+++ src/components/ProductCard.js
@@ -2,8 +2,11 @@ import React from "react";
 import { Card } from "react-bootstrap";
 
 function ProductCard({ product }) {
+  const imageUrl = product.imagenUrl || "https://via.placeholder.com/150";
+
   return (
     <Card className="mb-3 shadow-sm">
+      <Card.Img variant="top" src={imageUrl} alt={product.nombreProducto} /> {/* 👈 Mostrar la imagen */}
       <Card.Body>
         <Card.Title>{product.nombreProducto}</Card.Title>
         <Card.Text>
